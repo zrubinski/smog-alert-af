0491f1531a52a84d85eac9d6a4ab3cf3dcfccde1
Zbigniew Rubinski <zrubinski@objectivity.co.uk>
2019-12-05 00:21:38 +0100

    Calling SmogAlertSerive

--- SmogAlertCron/index.ts
+++ SmogAlertCron/index.ts
@@ -1,13 +1,15 @@
 import { AzureFunction, Context } from "@azure/functions"
+import { SmogAlertService } from "../shared/SmogAlertService";
 
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
+
+    new SmogAlertService().checkAirlyIndex();
+
     var timeStamp = new Date().toISOString();
-    
-    if (myTimer.IsPastDue)
-    {
+    if (myTimer.IsPastDue) {
         context.log('Timer function is running late!');
     }
-    context.log('Timer trigger function ran!', timeStamp);   
+    context.log('Timer trigger function ran!', timeStamp);
 };
 
 export default timerTrigger;
